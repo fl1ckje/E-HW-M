b34b42bdcff4a25e245a246f71b50cce7ef8c08f
fl1ckje <fl1ckje@mail.ru>
2025-08-23 22:33:36 +0800

    Assembly data update. Added icon

--- E-HW-M/Properties/AssemblyInfo.cs
+++ E-HW-M/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("E-HW-M")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("External HW Monitor")]
+[assembly: AssemblyDescription("Simple desktop application, which sends hardware stats over serial port to device")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("fl1ckje")]
 [assembly: AssemblyProduct("E-HW-M")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("© 2025 All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
