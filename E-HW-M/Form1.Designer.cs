dd0c21e89f938d353090c9a9d27eb5b328b79724
fl1ckje <fl1ckje@mail.ru>
2025-08-23 22:28:16 +0800

    Add project files.

---
+++ E-HW-M/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace E_HW_M
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.comComboBox = new System.Windows.Forms.ComboBox();
+			this.comLabel = new System.Windows.Forms.Label();
+			this.connectionGroupBox = new System.Windows.Forms.GroupBox();
+			this.txDataTextBox = new System.Windows.Forms.TextBox();
+			this.txLabel = new System.Windows.Forms.Label();
+			this.connectButton = new System.Windows.Forms.Button();
+			this.updateComButton = new System.Windows.Forms.Button();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.connectionStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.connectionGroupBox.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// comComboBox
+			// 
+			this.comComboBox.DisplayMember = "ds";
+			this.comComboBox.FormattingEnabled = true;
+			this.comComboBox.Location = new System.Drawing.Point(69, 16);
+			this.comComboBox.Name = "comComboBox";
+			this.comComboBox.Size = new System.Drawing.Size(66, 21);
+			this.comComboBox.Sorted = true;
+			this.comComboBox.TabIndex = 0;
+			// 
+			// comLabel
+			// 
+			this.comLabel.AutoSize = true;
+			this.comLabel.Location = new System.Drawing.Point(6, 19);
+			this.comLabel.Name = "comLabel";
+			this.comLabel.Size = new System.Drawing.Size(57, 13);
+			this.comLabel.TabIndex = 1;
+			this.comLabel.Text = "Serial port:";
+			// 
+			// connectionGroupBox
+			// 
+			this.connectionGroupBox.Controls.Add(this.txDataTextBox);
+			this.connectionGroupBox.Controls.Add(this.txLabel);
+			this.connectionGroupBox.Controls.Add(this.connectButton);
+			this.connectionGroupBox.Controls.Add(this.updateComButton);
+			this.connectionGroupBox.Controls.Add(this.comLabel);
+			this.connectionGroupBox.Controls.Add(this.comComboBox);
+			this.connectionGroupBox.Location = new System.Drawing.Point(12, 12);
+			this.connectionGroupBox.Name = "connectionGroupBox";
+			this.connectionGroupBox.Size = new System.Drawing.Size(279, 73);
+			this.connectionGroupBox.TabIndex = 2;
+			this.connectionGroupBox.TabStop = false;
+			this.connectionGroupBox.Text = "Connection";
+			// 
+			// txDataTextBox
+			// 
+			this.txDataTextBox.Location = new System.Drawing.Point(69, 43);
+			this.txDataTextBox.Name = "txDataTextBox";
+			this.txDataTextBox.ReadOnly = true;
+			this.txDataTextBox.Size = new System.Drawing.Size(124, 20);
+			this.txDataTextBox.TabIndex = 5;
+			// 
+			// txLabel
+			// 
+			this.txLabel.AutoSize = true;
+			this.txLabel.Location = new System.Drawing.Point(7, 46);
+			this.txLabel.Name = "txLabel";
+			this.txLabel.Size = new System.Drawing.Size(56, 13);
+			this.txLabel.TabIndex = 4;
+			this.txLabel.Text = "Sent data:";
+			// 
+			// connectButton
+			// 
+			this.connectButton.Location = new System.Drawing.Point(199, 15);
+			this.connectButton.Name = "connectButton";
+			this.connectButton.Size = new System.Drawing.Size(74, 23);
+			this.connectButton.TabIndex = 3;
+			this.connectButton.Text = "Connect";
+			this.connectButton.UseVisualStyleBackColor = true;
+			this.connectButton.Click += new System.EventHandler(this.ConnectButtonClick);
+			// 
+			// updateComButton
+			// 
+			this.updateComButton.Location = new System.Drawing.Point(141, 15);
+			this.updateComButton.Name = "updateComButton";
+			this.updateComButton.Size = new System.Drawing.Size(52, 23);
+			this.updateComButton.TabIndex = 2;
+			this.updateComButton.Text = "Update";
+			this.updateComButton.UseVisualStyleBackColor = true;
+			this.updateComButton.Click += new System.EventHandler(this.UpdateComButtonClick);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionStatusLabel});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 91);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(303, 22);
+			this.statusStrip1.TabIndex = 3;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// connectionStatusLabel
+			// 
+			this.connectionStatusLabel.ForeColor = System.Drawing.Color.Red;
+			this.connectionStatusLabel.Name = "connectionStatusLabel";
+			this.connectionStatusLabel.Size = new System.Drawing.Size(79, 17);
+			this.connectionStatusLabel.Text = "Disconnected";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(303, 113);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.connectionGroupBox);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "E.HW.M.";
+			this.connectionGroupBox.ResumeLayout(false);
+			this.connectionGroupBox.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comComboBox;
+		private System.Windows.Forms.Label comLabel;
+		private System.Windows.Forms.GroupBox connectionGroupBox;
+		private System.Windows.Forms.Button updateComButton;
+		private System.Windows.Forms.Button connectButton;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel connectionStatusLabel;
+		private System.Windows.Forms.Label txLabel;
+		private System.Windows.Forms.TextBox txDataTextBox;
+	}
+}
+
---
+++ E-HW-M/Form1.cs
@@ -0,0 +1,257 @@
+﻿using OpenHardwareMonitor.Hardware;
+using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace E_HW_M
+{
+	public partial class MainForm : Form
+	{
+		private SerialPort serialPort;
+		private bool isConnected;
+		private Thread monitorThread;
+		private readonly object lockObject = new object();
+		private readonly Computer computer;
+		private UpdateVisitor updateVisitor;
+
+		private readonly string systemPath = Path.GetPathRoot(Environment.SystemDirectory);
+		private readonly double bytes2gbytes = Math.Pow(1024.0, 3);
+
+		public MainForm()
+		{
+			InitializeComponent();
+			UpdateComPortsList();
+
+			updateVisitor = new UpdateVisitor();
+			computer = new Computer
+			{
+				CPUEnabled = true,
+				GPUEnabled = true,
+				RAMEnabled = true,
+			};
+			computer.Open();
+		}
+
+		private void UpdateComPortsList()
+		{
+			if(comComboBox.InvokeRequired)
+			{
+				comComboBox.Invoke((MethodInvoker)delegate
+				{
+					UpdateComPortsList();
+				});
+				return;
+			}
+
+			comComboBox.Items.Clear();
+			foreach(string portName in SerialPort.GetPortNames())
+			{
+				comComboBox.Items.Add(portName);
+			}
+
+			if(comComboBox.Items.Count > 0)
+			{
+				comComboBox.Enabled = true;
+				connectButton.Enabled = true;
+			}
+			else
+			{
+				comComboBox.Items.Add("");
+				comComboBox.Enabled = false;
+				connectButton.Enabled = false;
+			}
+			comComboBox.SelectedIndex = 0;
+		}
+
+		private void InitializeSerialPort(string portName)
+		{
+			serialPort?.Dispose(); // Clean up existing port if any
+
+			serialPort = new SerialPort(portName)
+			{
+				BaudRate = 9600,
+				ReadTimeout = 500,
+				WriteTimeout = 500
+			};
+		}
+
+		private void MonitorStats()
+		{
+			while(isConnected)
+			{
+				string statsData = GetFormattedData();
+
+				lock(lockObject)
+				{
+					if(serialPort.IsOpen)
+					{
+						txDataTextBox.Text = statsData; // TODO: debug for thread interaction
+						serialPort.WriteLine(statsData);
+					}
+				}
+
+				Thread.Sleep(2100); // Update every second
+			}
+			Disconnect();
+		}
+
+
+		private string GetFormattedData()
+		{
+			updateVisitor = new UpdateVisitor();
+			computer.Accept(updateVisitor);
+
+			int? cpuLoad = null,
+				cpuTemp = null,
+				gpuLoad = null,
+				gpuTemp = null,
+				usedRam = null,
+				totalRam = null;
+			int usedSpace, totalSpace;
+
+			// Collect hardware data except drive info
+			foreach(IHardware hw in computer.Hardware)
+			{
+				switch(hw.HardwareType)
+				{
+					// CPU data collection
+					case HardwareType.CPU:
+						foreach(ISensor sensor in hw.Sensors)
+						{
+							if(sensor.SensorType == SensorType.Load && sensor.Name.Contains("Total"))
+								cpuLoad = NearestInt(sensor.Value);
+
+							if(sensor.SensorType == SensorType.Temperature)
+								cpuTemp = NearestInt(sensor.Value);
+						}
+						break;
+
+					// GPU data collection
+					case HardwareType.GpuNvidia:
+					case HardwareType.GpuAti:
+						foreach(ISensor sensor in hw.Sensors)
+						{
+							if(sensor.SensorType == SensorType.Load)
+								gpuLoad = NearestInt(sensor.Value);
+
+							if(sensor.SensorType == SensorType.Temperature)
+								gpuTemp = NearestInt(sensor.Value);
+						}
+						break;
+
+					// RAM data collection
+					case HardwareType.RAM:
+						foreach(ISensor sensor in hw.Sensors)
+						{
+							if(sensor.SensorType == SensorType.Data)
+							{
+								if(sensor.Name.Contains("Used"))
+									usedRam = NearestInt(sensor.Value);
+								if(sensor.Name.Contains("Available"))
+									totalRam = usedRam + NearestInt(sensor.Value);
+							}
+						}
+						break;
+					default:
+						break;
+				}
+			}
+
+			DriveInfo drive = new DriveInfo(systemPath);
+			usedSpace = (int)(drive.AvailableFreeSpace / bytes2gbytes);
+			totalSpace = (int)(drive.TotalSize / bytes2gbytes);
+
+
+			// Format the string according to requirements
+			return $"{cpuTemp?.ToString() ?? "-1"};" +
+				   $"{cpuLoad?.ToString() ?? "-1"};" +
+				   $"{gpuTemp?.ToString() ?? "-1"};" +
+				   $"{gpuLoad?.ToString() ?? "-1"};" +
+				   $"{usedRam?.ToString() ?? "-1"};" +
+				   $"{totalRam?.ToString() ?? "-1"};" +
+				   $"{usedSpace};" +
+				   $"{totalSpace}";
+		}
+
+		protected override void OnFormClosing(FormClosingEventArgs e)
+		{
+			Disconnect();
+			computer.Close();
+			base.OnFormClosing(e);
+		}
+
+		private void UpdateComButtonClick(object sender, EventArgs e) => UpdateComPortsList();
+
+		private void ConnectButtonClick(object sender, EventArgs e)
+		{
+			if(!isConnected)
+			{
+				try
+				{
+					string selectedPort = comComboBox.SelectedItem?.ToString();
+					InitializeSerialPort(selectedPort);
+					serialPort.Open();
+					isConnected = true;
+					connectButton.Text = "Disconnect";
+					connectionStatusLabel.ForeColor = Color.Green;
+					connectionStatusLabel.Text = "Connected";
+
+					// Start monitoring thread
+					monitorThread = new Thread(MonitorStats);
+					monitorThread.Start();
+				}
+				catch(Exception ex)
+				{
+					MessageBox.Show($"Error connecting to board: {ex.Message}");
+				}
+			}
+			else
+			{
+				Disconnect();
+			}
+		}
+
+		private void Disconnect()
+		{
+			isConnected = false;
+			if(monitorThread != null && monitorThread.IsAlive)
+			{
+				monitorThread.Join(1000);
+			}
+
+			if(serialPort != null && serialPort.IsOpen)
+			{
+				serialPort?.Close();
+				serialPort?.Dispose();
+			}
+
+			connectButton.Text = "Connect";
+			connectionStatusLabel.ForeColor = Color.Red;
+			connectionStatusLabel.Text = "Disconnected";
+
+			UpdateComPortsList();  // Refresh ports after disconnection
+		}
+
+		private int? NearestInt(float? val) => (int?)Math.Round((double)val, 0);
+	}
+}
+
+public class UpdateVisitor : IVisitor
+{
+	public void VisitComputer(IComputer computer)
+	{
+		computer.Traverse(this);
+	}
+
+	public void VisitHardware(IHardware hardware)
+	{
+		hardware.Update();
+		foreach(IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+	}
+
+	public void VisitSensor(ISensor sensor) { }
+	public void VisitParameter(IParameter parameter) { }
+}
